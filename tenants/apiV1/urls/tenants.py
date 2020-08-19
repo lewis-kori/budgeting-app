c1e19486892b0b06ddc0c0467b8f92f63fec54e7
lewis-kori <lewiskosh@gmail.com>
2020-08-19 22:46:23 +0300

    added tenant info endpoint

--- tenants/apiV1/urls/tenants.py
+++ tenants/apiV1/urls/tenants.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from ..views.tenants import TenantListCreateAPIView
+from ..views.tenants import TenantInfoAPIView, TenantListCreateAPIView
 
 app_name='tenants'
 
 urlpatterns = [
     path('tenants/', TenantListCreateAPIView.as_view(),name='tenants'),
+    path('tenants/info/', TenantInfoAPIView.as_view(),name='tenant_info'),
 ]
--- tenants/apiV1/views/tenants.py
+++ tenants/apiV1/views/tenants.py
@@ -1,9 +1,45 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.status import (HTTP_200_OK, HTTP_400_BAD_REQUEST,
+                                   HTTP_403_FORBIDDEN)
+from rest_framework.views import APIView
 
 from ...models import Tenant
 from ..serializers.tenants import TenantSerializer
 
 
+# retrieves a list of all tenants
 class TenantListCreateAPIView(ListCreateAPIView):
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
+
+
+# retrieves an instance of a tenant, use this for login credentials
+class TenantInfoAPIView(APIView):
+    def post(self, request, **kwargs):
+        data = request.data
+
+        if 'tenant_id' in data.keys():
+            tenant = get_object_or_404(Tenant, tenant_id=data['tenant_id'])
+
+            if tenant.is_active:
+                data = {
+                    'id': tenant.id,
+                    'subdomain': tenant.subdomain,
+                    'organization_id': tenant.tenant_id
+                }
+
+                return Response(data, status=HTTP_200_OK)
+            else:
+                return Response(
+                    {
+                        'detail':
+                        'It appears your organization is inactive, please contact support to rectify this.'
+                    },
+                    status=HTTP_403_FORBIDDEN)
+
+        else:
+            return Response(
+                {'detail': 'Please pass tenant_id as a key in your request'},
+                status=HTTP_400_BAD_REQUEST)
