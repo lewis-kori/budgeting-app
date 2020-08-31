5bd0ff05d036bd37c2d9d053a554f59d3318d50d
lewis-kori <lewiskosh@gmail.com>
2020-08-31 14:41:51 +0300

    modified root url

--- budgeting/urls.py
+++ budgeting/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('', admin.site.urls),
 
     # third party apps urls
     path('api/v1/auth/', include('djoser.urls')),
